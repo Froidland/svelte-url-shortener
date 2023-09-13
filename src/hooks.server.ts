9975366f1678e834c7c016216b0d65315328b078
Francisco Hauva <phauva@gmail.com>
2023-09-13 20:35:24 -0300

    save session to locals

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,7 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
-	// event.locals.session = await event.locals.auth.validate();
+	event.locals.session = await event.locals.auth.validate();
 
 	return await resolve(event);
 };
