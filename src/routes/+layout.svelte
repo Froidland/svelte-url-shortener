0b2967f565860ef81c1edc208c6d6655a2e78504
Francisco Hauva <phauva@gmail.com>
2023-10-03 04:40:25 -0300

    feat: new colors :D

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/button/button.svelte';
 	import '../app.css';
 	import type { LayoutServerData } from './$types';
 	import { DiscordIcon, Home, UserCircle, LogOut } from '$lib/components/icons';
@@ -14,32 +13,19 @@
 <div class="container">
 	<nav class="flex items-center justify-between bg-zinc-800 p-3 my-3 rounded">
 		<div class="flex">
-			<Button
-				variant="ghost"
-				class="flex  gap-2 rounded font-semibold text-sm hover:bg-zinc-700"
-				href="/"
-			>
+			<a class="btn btn-secondary gap-2" href="/">
 				<Home size="18" />Home
-			</Button>
+			</a>
 		</div>
 		<div class="flex gap-2">
 			{#if !data.isLoggedIn}
-				<a
-					href="/api/auth/login/discord"
-					class="flex gap-2 hover:bg-blue-900 text-white text-sm font-semibold py-2 px-4 rounded items-center transition-colors"
-				>
+				<a href="/api/auth/login/discord" class="btn btn-secondary gap-2">
 					<DiscordIcon size="24" />
 					Sign in with Discord
 				</a>
 			{:else}
-				<Button class="rounded hover:bg-zinc-700 flex gap-2" variant="ghost" href="/profile"
-					><UserCircle size="18" /> Profile</Button
-				>
-				<Button
-					class="rounded text-red-500 hover:bg-red-700 flex gap-2"
-					variant="ghost"
-					href="/api/auth/logout"><LogOut size="18" />Log out</Button
-				>
+				<a class="btn btn-secondary gap-2" href="/profile"><UserCircle size="18" /> Profile</a>
+				<a class="btn btn-destructive gap-2" href="/api/auth/logout"><LogOut size="18" />Log out</a>
 			{/if}
 		</div>
 	</nav>
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -108,8 +108,8 @@
 						>
 							<button
 								on:click={() => deleteEntry(url.slug)}
-								class="bg-red-800 py-2 px-2 rounded hover:bg-red-700 transition-colors"
-								><Trash2 size="18" /></button
+								class="bg-red-400 py-2 px-2 rounded hover:bg-red-300 transition-colors"
+								><Trash2 size="18" class="text-red-950" /></button
 							>
 						</Table.Cell>
 					</Table.Row>
