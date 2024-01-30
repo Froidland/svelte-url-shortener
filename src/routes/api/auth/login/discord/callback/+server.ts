13937376fe4e872a657ab5e7c2a9359ab7540fc2
Francisco Hauva <phauva@gmail.com>
2024-01-30 06:55:20 -0300

    fix: don't catch thrown sveltekit errors in order to let sveltekit itself handle them

--- src/routes/api/auth/login/discord/callback/+server.ts
+++ src/routes/api/auth/login/discord/callback/+server.ts
@@ -1,6 +1,7 @@
 import { db } from '$lib/server/db/index.js';
 import { users } from '$lib/server/db/schema.js';
 import { discordAuth, lucia } from '$lib/server/lucia.js';
+import { error, redirect } from '@sveltejs/kit';
 import { OAuth2RequestError } from 'arctic';
 import { eq } from 'drizzle-orm';
 import { generateId } from 'lucia';
@@ -16,76 +17,61 @@ export async function GET({ url, cookies }) {
 		});
 	}
 
+	let discordTokens;
 	try {
-		const discordTokens = await discordAuth.validateAuthorizationCode(code);
+		discordTokens = await discordAuth.validateAuthorizationCode(code);
+	} catch (e) {
+		console.error(e);
 
-		const res = await fetch('https://discord.com/api/users/@me', {
-			headers: {
-				Authorization: `Bearer ${discordTokens.accessToken}`
-			}
-		});
-
-		if (!res.ok) {
-			return new Response(null, {
-				status: 500
-			});
+		if (e instanceof OAuth2RequestError) {
+			error(400, e.message);
 		}
 
-		const discordUser = (await res.json()) as DiscordUser;
-
-		const existingUser = await db.query.users.findFirst({
-			where: eq(users.discordId, discordUser.id)
-		});
-
-		if (existingUser) {
-			const session = await lucia.createSession(existingUser.id, {});
-			const sessionCookie = lucia.createSessionCookie(session.id);
-			cookies.set(sessionCookie.name, sessionCookie.value, {
-				path: '.',
-				...sessionCookie.attributes
-			});
-
-			return new Response(null, {
-				status: 302,
-				headers: {
-					Location: '/'
-				}
-			});
+		error(500, 'Unexpected error.');
+	}
+	
+	const res = await fetch('https://discord.com/api/users/@me', {
+		headers: {
+			Authorization: `Bearer ${discordTokens.accessToken}`
 		}
+	});
 
-		const userId = generateId(24);
-		await db.insert(users).values({
-			id: userId,
-			discordId: discordUser.id,
-			discordUsername: discordUser.username
-		});
+	if (!res.ok) {
+		error(500, "Couldn't fetch user data from Discord.");
+	}
 
-		const session = await lucia.createSession(userId, {});
+	const discordUser = (await res.json()) as DiscordUser;
+
+	const existingUser = await db.query.users.findFirst({
+		where: eq(users.discordId, discordUser.id)
+	});
+
+	if (existingUser) {
+		const session = await lucia.createSession(existingUser.id, {});
 		const sessionCookie = lucia.createSessionCookie(session.id);
 		cookies.set(sessionCookie.name, sessionCookie.value, {
 			path: '.',
 			...sessionCookie.attributes
 		});
 
-		return new Response(null, {
-			status: 302,
-			headers: {
-				Location: '/'
-			}
-		});
-	} catch (error) {
-		console.error(error);
-
-		if (error instanceof OAuth2RequestError) {
-			return new Response(null, {
-				status: 400
-			});
-		}
-
-		return new Response(null, {
-			status: 500
-		});
+		redirect(302, '/');
 	}
+
+	const userId = generateId(24);
+	await db.insert(users).values({
+		id: userId,
+		discordId: discordUser.id,
+		discordUsername: discordUser.username
+	});
+
+	const session = await lucia.createSession(userId, {});
+	const sessionCookie = lucia.createSessionCookie(session.id);
+	cookies.set(sessionCookie.name, sessionCookie.value, {
+		path: '.',
+		...sessionCookie.attributes
+	});
+
+	redirect(302, '/');
 }
 
 type DiscordUser = {
