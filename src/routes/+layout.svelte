460ff7bfb7b87e61eb7fb11634de6af9b2d9ba4e
Francisco Hauva <phauva@proton.me>
2024-08-29 20:57:43 -0400

    fix: mobile friendly buttons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,10 +9,10 @@
 
 <Toaster />
 <div class="container">
-	<nav class="flex items-center justify-between bg-zinc-800 p-3 my-3 rounded">
+	<nav class="my-3 flex items-center justify-between rounded bg-zinc-800 p-3">
 		<div class="flex">
 			<a class="btn btn-secondary gap-2" href="/">
-				<Home size="18" />Home
+				<Home size="18" /> <span class="hidden sm:block">Home</span>
 			</a>
 		</div>
 		<div class="flex gap-2">
@@ -22,8 +22,12 @@
 					Sign in with Discord
 				</a>
 			{:else}
-				<a class="btn btn-secondary gap-2" href="/profile"><UserCircle size="18" /> Profile</a>
-				<a class="btn btn-destructive gap-2" href="/api/auth/logout"><LogOut size="18" />Log out</a>
+				<a class="btn btn-secondary gap-2" href="/profile"
+					><UserCircle size="18" /> <span class="hidden sm:block">Profile</span></a
+				>
+				<a class="btn btn-destructive gap-2" href="/api/auth/logout"
+					><LogOut size="18" /><span class="hidden sm:block">Log out</span></a
+				>
 			{/if}
 		</div>
 	</nav>
